5964a9f619baee65c3fd2d40db14052e4625881f
swpinkerton <swpinkerton11@gmail.com>
2023-03-12 17:46:22 +0000

    make test fail

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int fact(int n) {
 }
 
 TEST_CASE("testing the factorial function") {
-//    CHECK(fact(0) == 1); // should fail
+    CHECK(fact(0) == 1); // should fail
     CHECK(fact(1) == 1);
     CHECK(fact(2) == 2);
     CHECK(fact(3) == 6);
